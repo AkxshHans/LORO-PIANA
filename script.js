86c0488e09202782572f1f7f0b4751bc07d602b9
Akash Hans <162800417+AkxshHans@users.noreply.github.com>
2024-04-10 07:27:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.from("#navpart1 , #navpart2 , #navpart3", {
+
+y : -100 ,
+duration : 0.5 ,
+delay : 2,
+stagger : 1
+
+})
+
+
+gsap.from("#mainbottom", {
+
+    y : -900 ,
+    duration : 1 ,
+    delay : 7,
+    stagger : 1
+    
+    })
+
+
+    gsap.from("#videomain", {
+
+        y : -900 ,
+        duration : 2 ,
+        delay : 5,
+        stagger : 1 ,
+        opacity :  0.5        
+        
+        })
